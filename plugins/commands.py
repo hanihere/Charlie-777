0ca4a19ea6222402024922fec2acf33a4d86b65a
hanihere <115630779+hanihere@users.noreply.github.com>
2022-10-15 22:01:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -115,6 +115,8 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Join', url='https://t.me/h6nihere') ] ] ),
+
         )
                     
 
