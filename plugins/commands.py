8aebf2a5ed3a21f570b2bbf42826b8e34ebec12c
hanihere <115630779+hanihere@users.noreply.github.com>
2022-10-17 17:48:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -34,8 +34,6 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('Add me to your Group', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
             InlineKeyboardButton('Channel', url='https://t.me/H6niedits'),
             InlineKeyboardButton('Updates', url='https://t.me/H6nihere')
             ],[      
@@ -77,8 +75,6 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('Add me to your Group', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
             InlineKeyboardButton('Channel', url='https://t.me/H6niedits'),
             InlineKeyboardButton('Updates', url='https://t.me/H6nihere')
             ],[      
